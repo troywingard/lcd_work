58aa986e2664f58ae774fcad288378e068553e89
Deborah-Julicher <69861723+Deborah-Julicher@users.noreply.github.com>
2020-10-04 10:41:08 -0700

    I added another custom character to the change sequence

--- Custom_Character_2/Custom_Character_2.ino
+++ Custom_Character_2/Custom_Character_2.ino
@@ -24,28 +24,44 @@ byte customChar1[8] = {
   0b11111,
 };
 
+byte customChar2[8] = {
+  0b00100,
+  0b00100,
+  0b00100,
+  0b00100,
+  0b11111,
+  0b11111,
+  0b11111,
+  0b11111,
+};
+
 
 void setup() 
 {
+  lcd.init();
+  lcd.backlight();
   
-
+  lcd.createChar(0, customChar);
+  lcd.createChar(1, customChar1);
+  lcd.createChar(2, customChar2);
 }
 
 void loop() 
 {
  
-  lcd.init();
-  lcd.backlight();
- 
-
-  lcd.createChar(0, customChar);
-  lcd.createChar(1, customChar1);
-
-
   lcd.setCursor(2, 0);
   lcd.write((byte)0);
+  
   delay(500);
+  
   lcd.setCursor(2, 0);
   lcd.write((byte)1);
+
+  delay(500);
+
+  lcd.setCursor(2, 0);
+  lcd.write((byte)2);
+
+  delay(500);
   
 }
